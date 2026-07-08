a2f325d224ce5c583d40cd720130acc6a35a2f6a
mehranebi21 <mehranebi22g2@gmail.com>
2026-07-08 21:26:36 +0330

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,34 +1,55 @@
+const API_KEY = "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";
+
+
 export async function askAI(message){
 
-
-let response=
-await fetch(
-"https://YOUR_AI_API_URL",
+const response = await fetch(
+"https://openrouter.ai/api/v1/chat/completions",
 {
 
 method:"POST",
 
 headers:{
+
+"Authorization":
+`Bearer ${API_KEY}`,
+
 "Content-Type":
 "application/json"
-},
 
+},
 
 body:JSON.stringify({
 
-message
+model:
+"qwen/qwen-2.5-coder-32b-instruct",
+
+messages:[
+
+{
+role:"user",
+content:message
+}
+
+]
 
 })
 
+}
 
-});
-
-
-let data=
-await response.json();
+);
 
 
-return data.answer;
+const data = await response.json();
 
+
+if(data.choices){
+
+return data.choices[0].message.content;
+
+}
+
+
+return "خطا در دریافت پاسخ از مدل";
 
 }
